8dd74883a2b5771e666e58810c6f233fb99ebd3c
Camilaufu <camilarossignatti@hotmail.com>
2016-09-08 12:51:53 -0300

    fim de aula

--- aula20160908/vet2.c
+++ aula20160908/vet2.c
@@ -6,23 +6,24 @@ int main ()
 {
 
     int numeros [N],rev [N];
-      int i, j;
+      int i, j=0;
     printf(" digite 10 Numeros inteiros nao negativo\n");
 
 
     for (i=0;i<N;i++)
     {
-     scanf("%d",&numeros[N]);
+     scanf("%d",&numeros[i]);
     }
 
 
-    for (i=N;i>0;i--)
-    for (j=i;j<N;j++)
+    for (i=N-1;i>=0;i--)
+
     {
       rev [j] = numeros[i];
+      j++;
     }
 
-    for (i=0;i<N;i++)
+    for (j=0;j<N;j++)
           printf("vetor: %d\n", rev [j]);
 
     return 0;
